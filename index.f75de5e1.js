29f13a5632c6cc0e54bbce4cd178b94a8cbb3a2e
Oleksandr Robenko <oleksandr.robenko@gmail.com>
2024-08-12 23:08:36 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,11 @@
+"use strict";
+const countries = [
+    ...document.querySelectorAll("span.population")
+];
+const population = countries.map((country)=>+country.textContent.trim().replace(/,/g, ""));
+const total = population.reduce((acumulator, currentValue)=>acumulator + currentValue, 0);
+const avarage = total / countries.length;
+document.querySelector(".total-population").replaceWith(total.toLocaleString("en-US"));
+document.querySelector(".average-population").replaceWith(avarage.toLocaleString("en-US"));
+
+//# sourceMappingURL=index.f75de5e1.js.map
